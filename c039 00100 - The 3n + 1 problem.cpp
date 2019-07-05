e3ed416390bd57075a8ea87199da144d8a76385e
Your Name <you@example.com>
2019-07-05 17:04:23 +0800

    four commit

---
+++ c039 00100 - The 3n + 1 problem.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	int n1, n2;  
+
+	while (cin >> n1 >> n2)
+	{
+		cout << n1 << " " << n2 << " ";
+		int temp = n1;
+		n1 > n2 ? n1 = n2, n2 = temp : n1;
+		if (0 < n1&&n1 < 1000000 && 0 < n2&&n2 < 1000000)
+		{
+			int max = 0;
+			for (int i = n1; i <= n2; i++)
+			{
+				int j = i, count = 1;
+				while (j != 1)
+				{
+					if (j % 2 == 1)
+					{
+						j = 3 * j + 1;
+					}
+					else
+					{
+						j /= 2;
+					}
+					count++;
+				}
+				max < count ? max = count : max;
+			}
+			cout << max << endl;
+		}
+	}
+
+	system("pause");
+	return 0;
+}
+
